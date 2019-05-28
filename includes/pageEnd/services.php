bca2ba681524a99d699859f8be21b5fb1aa9ef3f
Gokul199602 <41792933+Gokul199602@users.noreply.github.com>
2019-05-28 18:12:23 +0530

    Services

---
+++ includes/pageEnd/services.php
@@ -0,0 +1,8 @@
+<div class="serviceContainer">
+    <h1>Services</h1>
+    <p><i class="fa fa-plane fa-2x" aria-hidden="true" style="color:#33ff9c"></i>     We provide Services for flight tickets and a dedicated support is given to it</p>
+    <p><i class="fa fa-train fa-2x" aria-hidden="true" style="color:#33ff9c"></i>     The train ticket will be booked on the time you want and desire</p>
+    <p><i class="fa fa-bus   fa-2x" aria-hidden="true" style="color:#33ff9c"></i>     The bus tickets will be booked on time and fast at any time</p>
+    <p><i class="fa fa-helicopter fa-2x"></i>     We provide Services for flight tickets and a dedicated support is given to it</p>
+    <p><i class="fa fa-plane fa-2x" aria-hidden="true" style="color:#33ff9c"></i>     We provide Services for flight tickets and a dedicated support is given to it</p>
+</div>
