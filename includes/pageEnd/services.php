4a7d46a8706877dbb1f5ad60d2ef8c39ae6d5599
Gokul199602 <41792933+Gokul199602@users.noreply.github.com>
2019-05-29 09:22:57 +0530

    The new changes

--- includes/pageEnd/services.php
+++ includes/pageEnd/services.php
@@ -3,6 +3,16 @@
     <p><i class="fa fa-plane fa-2x" aria-hidden="true" style="color:#33ff9c"></i>     We provide Services for flight tickets and a dedicated support is given to it</p>
     <p><i class="fa fa-train fa-2x" aria-hidden="true" style="color:#33ff9c"></i>     The train ticket will be booked on the time you want and desire</p>
     <p><i class="fa fa-bus   fa-2x" aria-hidden="true" style="color:#33ff9c"></i>     The bus tickets will be booked on time and fast at any time</p>
-    <p><i class="fa fa-helicopter fa-2x"></i>     We provide Services for flight tickets and a dedicated support is given to it</p>
-    <p><i class="fa fa-plane fa-2x" aria-hidden="true" style="color:#33ff9c"></i>     We provide Services for flight tickets and a dedicated support is given to it</p>
+    <p><i class="fa fa-taxi fa-2x" aria-hidden="true" style="color:#33ff9c"></i>      We will provide immediate taxi service after your flight, train, or a bus</p>
+    <div class="serviceBottom">
+        <p>We provide best services in India and around the world. There are millions of 
+            of satisfied customers around the globe who used our service. We could able to 
+            make the process fast and seamless, so that the users don't have to wait for 
+            any time consuming process and can process it fast. There will be full support
+            from our team to work across any issue you face during the process.
+        </p>
+    </div>
+    <div class="rightContainer">
+        <img src="../../assets/images/travel.png" alt="">
+    </div>
 </div>
