0717085b77532f0572238004153e614d142f3590
Gokul199602 <41792933+Gokul199602@users.noreply.github.com>
2019-06-01 16:52:49 +0530

    A simple change
    A simple change

--- register.php
+++ register.php
@@ -8,20 +8,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script src="assets/js/script.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel = "stylesheet" type = "text/css" href = "assets/css/owl.carousel.min.css" />
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
      rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+     <link href="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.css" rel="stylesheet"/>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.0/jquery.min.js"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.min.js"></script>
     <title>Manju Tickets</title>
 </head>
 <body>
     <nav>
         <ul>
             <span onclick="openPage('includes/pageEnd/contact.php')"><li id="contactUs"><i class="fa fa-address-book"></i>Contact Us</li></span>
-            <span><li id="services"><i class="fa fa-taxi"></i>Services</li></span>
+            <span onclick="openPage('includes/pageEnd/services.php')"><li id="services"><i class="fa fa-taxi"></i>Services</li></span>
             <span><li id="header">Manju Tickets</li></span>
-            <span><li id="login"><i class="fas fa-sign-in-alt"></i> Login</li></span>
-            <span><li id="about"><i class="fa fa-address-card"></i>About</li></span>
+            <span onclick="openPage('register.php')"><li id="contactUs"><i class="fa fa-user" aria-hidden="true"></i> Login</li></span>
+            <span onclick="openPage('includes/pageEnd/about.php')"><li id="about"><i class="fa fa-address-card"></i>About</li></span>
         </ul>
     </nav>
     <div id="mainContainer">
@@ -44,5 +50,15 @@
             </div>
         </div>
     </div>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script
+    src="https://code.jquery.com/jquery-3.4.1.min.js"
+    integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+    crossorigin="anonymous"></script>
+    <script type="text/javascript" src = "assets/js/owl.carousel.min.js"></script>
+    <script src="assets/js/script.js"></script>
+    
 </body>
 </html>
